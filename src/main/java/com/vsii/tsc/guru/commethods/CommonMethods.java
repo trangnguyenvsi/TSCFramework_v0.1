87c5373124fdf5e28201287b7388ad61cfaf6c04
TRANGNTT <TRANGNTT@trangntt-w7.vsii.com>
2016-02-23 17:03:27 +0700

    Trangntt commit

---
+++ src/main/java/com/vsii/tsc/guru/commethods/CommonMethods.java
@@ -0,0 +1,17 @@
+package com.vsii.tsc.guru.commethods;
+
+import org.openqa.selenium.By;
+
+import com.vsii.tsc.guru.pages.method.LoginPageMethod;
+import com.vsii.tsc.guru.testbase.TestBase;
+
+public class CommonMethods {
+  //Check login or not
+	public static void checkLogin(){
+		LoginPageMethod objLogin;
+		objLogin = new LoginPageMethod(TestBase.driver);
+		if(TestBase.driver.findElements(By.name("uid")).size()!=0){
+			objLogin.loginToManagerPage("mngr30380", "vAtepYs");
+		}
+	}
+}
