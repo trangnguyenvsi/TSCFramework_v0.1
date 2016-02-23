2301d3a8bbc5f24d21912754c4da59633dfd97c1
TRANGNTT <TRANGNTT@trangntt-w7.vsii.com>
2016-02-23 17:01:45 +0700

    trangntt commit

--- src/main/java/com/vsii/tsc/guru/utility/CommonOperations.java
+++ src/main/java/com/vsii/tsc/guru/utility/CommonOperations.java
@@ -1,21 +1,28 @@
 package com.vsii.tsc.guru.utility;
 
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.Properties;
+import javax.imageio.ImageIO;
 
-import org.apache.commons.collections.MultiHashMap;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.vsii.tsc.guru.report.ExtentReporterNG;
 import com.vsii.tsc.guru.testbase.TestBase;
 
 @SuppressWarnings("deprecation")
@@ -23,13 +30,32 @@ public class CommonOperations {
 
 	List<String> list = null;
 
-	// Close Alert
+	/*
+	 * Read from configuration file
+	 */
+	public static Properties readConfig() throws IOException {
+		// Create new properties variable
+		Properties p = new Properties();
+		// Read object properties file
+		InputStream stream = new FileInputStream("./properties/config.properties");
+		// Load input stream file
+		p.load(stream);
+		return p;
+	}
+
+	//---------------------------------------------------------------------------
+	/*
+	 * Accept Alert
+	 */
+	
 	public static void closePopup() {
 		Alert alert = TestBase.driver.switchTo().alert();
 		alert.accept();
 	}
-
-	// Get Alert
+	//---------------------------------------------------------------------------
+	/*
+	 *  Get Alert
+	 */
 	public static boolean isAlertPresent(WebDriver driver) {
 		boolean foundAlert = false;
 		WebDriverWait wait = new WebDriverWait(driver,
@@ -42,54 +68,66 @@ public class CommonOperations {
 		}
 		return foundAlert;
 	}
-
-	// Check duplicated in list
-	public static boolean checkDuplicate(List<String> methodList) {
-		HashSet set = new HashSet();
-		for (int i = 0; i < methodList.size(); i++) {
-			boolean val = set.add(methodList.get(i));
-			if (val == false) {
-				return val;
-			}
-		}
-		return true;
-	}
-
-	// Count duplicated Value in list
-	public static int countDuplicatedValue(List<String> methodList, String searchText) {
-		int i;
-		for (i = 0; i < methodList.size(); i++) {
-			for (String s : methodList) {
-				if (methodList.get(i).equals(searchText))
-					;
-				i++;
-			}
-		}
-		return i;
-
-	}
-
-	// Take picture after test
+	//---------------------------------------------------------------------------
+	/*
+	 * Take picture after test
+	 */
 	public static void takePicture() throws Exception {
 		TestBase.imageName = TestBase.methodName + "_" + DateTime.createDateText();
 		// Capture popup
 		if (CommonOperations.isAlertPresent(TestBase.driver)) {
 			String dir = TestBase.p.getProperty("imagePath");
-			CreateDirectory.createDir(dir);
-			ExtentReporterNG.captureScreenShotPopUp(dir + TestBase.imageName);
+			createDir(dir);
+			captureScreenShotPopUp(dir + TestBase.imageName);
 
 			createTestCaseList();
 			closePopup();
 		}
 		// Capture screen
 		else {
-			ExtentReporterNG.CaptureScreen(TestBase.driver, TestBase.imageName);
+			CaptureScreen(TestBase.driver, TestBase.imageName);
 			createTestCaseList();
 		}
 		System.out.println("Image List Size" + TestBase.imageList.size());
 
 	}
-	//Create the test case image list
+	/*
+	 * Capture screen
+	 */
+	public static String CaptureScreen(WebDriver driver, String captureName) throws IOException {
+		Properties p = readConfig();
+		String imagePath;
+		String imageName;
+		imagePath = p.getProperty("imagePath") + captureName;
+		TakesScreenshot oScn = (TakesScreenshot) driver;
+		File oScnShot = oScn.getScreenshotAs(OutputType.FILE);
+		File oDest = new File(imagePath + ".jpg");
+		try {
+			FileUtils.copyFile(oScnShot, oDest);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		imageName = imagePath + ".jpg";
+		return imageName;
+}
+	/*
+	 * Capture screenshot
+	 */
+	public static String captureScreenShotPopUp(String img) throws Exception {
+		BufferedImage bfImage = new Robot()
+				.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+		ImageIO.write(bfImage, "png", new File(img + ".jpg"));
+		String imageName = img + ".jpg";
+		return imageName;
+
+}
+
+	
+	
+	//-----------------------------------------------------------------------------
+	/*
+	 * Create the test case's image list
+	 */
 	private static void createTestCaseList() {
 		if (TestBase.tcList.size() == 0) {
 			TestBase.imageList = new ArrayList<String>();
@@ -105,8 +143,10 @@ public class CommonOperations {
 			}
 		}
 	}
-
-	// Verify Element Present
+	//-------------------------------------------------------------------------------
+	/*
+	 *  Verify Element Present
+	 */
 	public static boolean verifyElementPresent(WebElement element) {
 
 		if (element.isDisplayed()) {
@@ -114,5 +154,17 @@ public class CommonOperations {
 		} else
 			return false;
 
+	}
+	//-------------------------------------------------------------------------------
+	/*
+	 * Create Directory
+	 */
+	public static void createDir(String dir){
+		String directoryName = dir;
+		File theDir = new File(directoryName);
+		if(!theDir.exists())
+		{
+			theDir.mkdir();
+		}
 	}
 }
